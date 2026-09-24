788e3292aa16bcc1759864e98ce550314cc289e5
RISHAV KAMAL <rishavkamalbg821@gmail.com>
2026-09-24 20:25:11 +0530

    Add initial spring boot backend

---
+++ backend/src/main/java/com/rishav/behindthecode/BehindTheCodeApplication.java
@@ -0,0 +1,13 @@
+package com.rishav.behindthecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehindTheCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BehindTheCodeApplication.class, args);
+	}
+
+}
